2fd702e1005965fb976d06998cfa9eb1c0f143ee
domdomxy <edemwerhani@gmail.com>
2026-09-18 13:45:07 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
